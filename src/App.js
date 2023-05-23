e61a45a29383cec86cf406e2ba86f370ee765a83
akashmurali <iamlittleakash@gmail.com>
2023-05-23 20:00:16 +0530

    project creating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+// import HomePage from "./ProjectPage/HomePage";
+import LoginPage from "./Components/LoginPage";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Nav";
+import ViewPage from "./Components/ViewPage";
+import { createContext, useContext, useState } from "react";
+import HomePage from "./Components/HomePage";
+
+export const SelectedContext = createContext(null);
 
 function App() {
+  // const [select, setSelect] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <HomePage /> */}
+      {/* <HomePage/> */}
+      <SelectedContext.Provider>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="homepage" element={<HomePage />} />
+          <Route path="viewpage" element={<ViewPage />}></Route>
+        </Routes>
+      </SelectedContext.Provider>
+      {/* <SelectedContext.Provider> */}
+      {/* <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="homepage" element={<HomePage />} />
+          <Route path="viewpage" element={<ViewPage />}></Route>
+        </Routes> */}
+      {/* </SelectedContext.Provider> */}
     </div>
   );
 }
